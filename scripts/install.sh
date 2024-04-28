89a3df6ad5870066d0cb86ccd2a38e2a21d9f0e4
shucheng <shucheng@bd-apaas.com>
2024-04-28 15:52:46 +0800

    v26.1.0

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,7 +9,7 @@ HTTP_SERVER="${HTTP_SERVER:-https://cache.wodcloud.com}"
 # 平台架构
 TARGET_ARCH="${TARGET_ARCH:-amd64}"
 # DOCKER版本
-DOCKER_VERSION="${DOCKER_VERSION:-24.0.9}"
+DOCKER_VERSION="${DOCKER_VERSION:-26.1.0}"
 
 LOCAL_ARCH=$(uname -m)
 if [ "$LOCAL_ARCH" = "x86_64" ]; then
