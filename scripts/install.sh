c1ea8375cc0cb41ee7063f44b08021a2981e4cc5
shucheng <shucheng@bd-apaas.com>
2025-07-15 10:40:39 +0800

    v28.3.2

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,7 @@ set -ex
 # 平台架构
 TARGET_ARCH="${TARGET_ARCH:-amd64}"
 # DOCKER版本
-DOCKER_VERSION="${DOCKER_VERSION:-28.2.2}"
+DOCKER_VERSION="${DOCKER_VERSION:-28.3.2}"
 
 LOCAL_ARCH=$(uname -m)
 if [ "$LOCAL_ARCH" = "x86_64" ]; then
