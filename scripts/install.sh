565572c37030dab9be64956c1a2f0902aa471961
shucheng <shucheng@bd-apaas.com>
2025-12-07 19:34:21 +0800

    fix /usr/local/bin

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,34 @@ set -ex
 # DOCKER版本
 DOCKER_VERSION="${DOCKER_VERSION:-28.3.2}"
 
-mkdir -p /opt/bin /opt/docker /opt/cni/bin
+LOCAL_ARCH=$(uname -m)
+if [ "$LOCAL_ARCH" = "x86_64" ]; then
+  TARGET_ARCH="amd64"
+elif [ "$(echo $LOCAL_ARCH | head -c 5)" = "armv8" ]; then
+  TARGET_ARCH="arm64"
+elif [ "$LOCAL_ARCH" = "aarch64" ]; then
+  TARGET_ARCH="arm64"
+elif [ "$LOCAL_ARCH" = "loongarch64" ]; then
+  TARGET_ARCH="loong64"
+else
+  TARGET_ARCH="unsupported"
+fi
+if [ "$LOCAL_ARCH" = "unsupported" ]; then
+  echo "This system's architecture ${LOCAL_ARCH} isn't supported"
+  exit 0
+fi
+
+# if [ -e /opt/bin/docker ]; then
+#   # 获取当前 Docker 版本
+#   current_version=$(/opt/bin/docker version --format '{{.Server.Version}}')
+#   # 比较版本
+#   if [ "$current_version" = "${DOCKER_VERSION}-beagle" ]; then
+#     echo "版本一致 $current_version , 无需安装."
+#     exit 0
+#   fi
+# fi
+
+mkdir -p /opt/bin /opt/docker /opt/cni/bin /usr/local/bin
 
 if ! [ $(getent group docker) ]; then
   groupadd docker
